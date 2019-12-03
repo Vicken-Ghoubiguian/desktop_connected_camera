6ae79dcf6cbc9be8148714d27431432924168dfd
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-12-03 13:10:21 +0100

    Correction of bug concerning photo shooting

--- src/photography_management_module.py
+++ src/photography_management_module.py
@@ -1,7 +1,7 @@
 import cv2
 from pygame import mixer
 
-def shoot_a_photo_function(desired_title, desired_format, desired_frame, desired_audio_file = '/home/eric/connected_camera/sounds/1_second_long_old_camera_sound.mp3'):
+def shoot_a_photo_function(desired_title, desired_format, desired_frame, desired_audio_file = 'sounds/1_second_long_old_camera_sound.mp3'):
 
 	image_formats_list = ['.jpg', '.jpeg', '.jpe', '.jp2', '.png', '.tiff', '.tif', '.bmp', '.dib', '.pbm', '.pgm', '.ppm']
 
