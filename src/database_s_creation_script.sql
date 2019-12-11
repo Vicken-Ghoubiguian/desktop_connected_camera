9f36f2a1eb56b9fbea9f9e219e05a6d7b37b35ef
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-12-11 12:52:32 +0100

    Completage of SQL script

--- src/database_s_creation_script.sql
+++ src/database_s_creation_script.sql
@@ -28,3 +28,24 @@ CREATE TABLE IF NOT EXISTS supported_video_formats_table(
 -- Insertion of video formats --
 INSERT INTO supported_video_formats_table(video_format_extension, associated_fourcc) VALUES(".avi", "XVID");
 INSERT INTO supported_video_formats_table(video_format_extension, associated_fourcc) VALUES(".mp4", "MP4V");
+
+-- Creation of the sound_effect_files_to_shot_photos_table table --
+CREATE TABLE IF NOT EXISTS sound_effect_files_to_shot_photos_table(
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	file_path VARCHAR(100) NOT NULL,
+	file_name VARCHAR(100) NOT NULL
+);
+
+-- Insertion of sound effect files to shot photos --
+INSERT INTO supported_image_formats_table(file_path, file_name) VALUES("sounds/4_seconds_long_paparazzi_camera_sound.mp3", "Paparazzi camera sound");
+INSERT INTO supported_image_formats_table(file_path, file_name) VALUES("sounds/1_second_long_old_camera_sound.mp3", "Old camera sound");
+
+-- Creation of the sound_effect_files_to_record_videos_table table --
+CREATE TABLE IF NOT EXISTS sound_effect_files_to_record_videos_table(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        file_path VARCHAR(100) NOT NULL,
+        file_name VARCHAR(100) NOT NULL
+);
+
+-- Insertion of sound effect files to shot photos --
+INSERT INTO supported_image_formats_table(file_path, file_name) VALUES("sounds/16_seconds_old_video_projector_sound.mp3", "Old video projector sound");
