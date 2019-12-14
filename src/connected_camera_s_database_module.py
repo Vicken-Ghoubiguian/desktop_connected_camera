d3e2ef68647d5f9903d8cac9c9c1f949a5edf51f
Eric GHOUBIGUIAN <eric.ghoubiguian@imerir.com>
2019-12-14 22:45:28 +0100

    Correction of a bug to shot videos

--- src/connected_camera_s_database_module.py
+++ src/connected_camera_s_database_module.py
@@ -1,5 +1,37 @@
 import sqlite3
 
+def selection_of_desired_fourcc_function(desired_format):
+
+	desired_fourcc = None
+
+	try:
+
+		connected_camera_s_database_connexion = sqlite3.connect('src/connected_camera_s_database.db')
+
+		connexion_s_cursor = connected_camera_s_database_connexion.cursor()
+
+		sql_query_string = "SELECT associated_fourcc FROM supported_video_formats_table WHERE video_format_extension = '{}'".format(desired_format)
+
+		connexion_s_cursor.execute(sql_query_string)
+
+		sql_query_result = connexion_s_cursor.fetchone()
+
+		desired_fourcc = sql_query_result[0]
+
+		connexion_s_cursor.close()
+
+	except sqlite3.Error as SQLError:
+
+		print("SQL Error: ", SQLError)
+
+	finally:
+
+		if(connected_camera_s_database_connexion):
+
+			connected_camera_s_database_connexion.close()
+
+		return desired_fourcc
+
 def verification_of_datas_existence_function(sough_field, concerned_table, concerned_field, concerned_data):
 
 	data_exists = False
--- src/videoWriter_module.py
+++ src/videoWriter_module.py
@@ -1,4 +1,5 @@
 import cv2
+import src.connected_camera_s_database_module as database_module
 from pygame import mixer
 
 def initialisation_of_videoWriter_function(desired_title, desired_format, desired_audio_file = 'sounds/16_seconds_old_video_projector_sound.mp3'):
@@ -9,7 +10,9 @@ def initialisation_of_videoWriter_function(desired_title, desired_format, desire
 
 	mixer.music.play()
 
-	fourcc = cv2.VideoWriter_fourcc(*fourcc_list[desired_format])
+	desired_fourcc = database_module.selection_of_desired_fourcc_function(desired_format)
+
+	fourcc = cv2.VideoWriter_fourcc(*desired_fourcc)
 
 	return cv2.VideoWriter(desired_title + desired_format, fourcc, 20.0, (640,480))
 
